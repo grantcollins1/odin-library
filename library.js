217a177388ea3045060474e5e8e84cf66b1ede98
gcollins <grant.collins@codeware.com>
2025-08-05 16:53:28 +0000

    create working layout displaying books

--- library.js
+++ library.js
@@ -1,7 +1,3 @@
-const p = document.createElement("p");
-p.textContent = "hello";
-document.body.appendChild(p);
-
 const myLibrary = [];
 
 function Book(author, title, numPages, read) {
@@ -16,3 +12,29 @@ function addBooktoLibrary(author, title, numPages, read) {
   const book = new Book(author, title, numPages, read);
   myLibrary.push(book);
 }
+
+function displayBooks() {
+  myLibrary.map((book) => {
+    console.log("book");
+    const bookContainer = document.createElement("div");
+    const authorDisplay = document.createElement("p");
+    authorDisplay.textContent = "Author: " + book.author;
+    bookContainer.appendChild(authorDisplay);
+    const pageNumDisplay = document.createElement("p");
+    pageNumDisplay.textContent = "Number of Pages: " + book.numPages;
+    bookContainer.appendChild(pageNumDisplay);
+    const titleDisplay = document.createElement("p");
+    titleDisplay.textContent = "Title: " + book.title;
+    bookContainer.appendChild(titleDisplay);
+    const readDisplay = document.createElement("p");
+    readDisplay.textContent = book.read ? "Has been read" : "Has not been read";
+    bookContainer.appendChild(readDisplay);
+    document.body.appendChild(bookContainer);
+
+  })
+}
+addBooktoLibrary("Grant", "To Kill a Mockingbird", 357, false);
+addBooktoLibrary("Jason Brown", "Cookbook", 10, true);
+addBooktoLibrary("Ben Weider", "Recipes", 150, true);
+displayBooks();
+
