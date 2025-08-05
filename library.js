d62f71f7839477c2b01b7532a1d7ca7c37c62236
gcollins <grant.collins@codeware.com>
2025-08-05 20:02:06 +0000

    add modal

--- library.js
+++ library.js
@@ -18,15 +18,15 @@ function displayBooks() {
   myLibrary.map((book) => {
     console.log("book");
     const bookContainer = document.createElement("div");
+    const titleDisplay = document.createElement("p");
+    titleDisplay.textContent = "Title: " + book.title;
+    bookContainer.appendChild(titleDisplay);
     const authorDisplay = document.createElement("p");
     authorDisplay.textContent = "Author: " + book.author;
     bookContainer.appendChild(authorDisplay);
     const pageNumDisplay = document.createElement("p");
     pageNumDisplay.textContent = "Number of Pages: " + book.numPages;
     bookContainer.appendChild(pageNumDisplay);
-    const titleDisplay = document.createElement("p");
-    titleDisplay.textContent = "Title: " + book.title;
-    bookContainer.appendChild(titleDisplay);
     const readDisplay = document.createElement("p");
     readDisplay.textContent = book.read ? "Has been read" : "Has not been read";
     bookContainer.appendChild(readDisplay);
@@ -35,7 +35,12 @@ function displayBooks() {
   })
 }
 addBooktoLibrary("Grant", "To Kill a Mockingbird", 357, false);
-addBooktoLibrary("Jason Brown", "Cookbook", 10, true);
-addBooktoLibrary("Ben Weider", "Recipes", 150, true);
+addBooktoLibrary("Jason", "Cookbook", 10, true);
+addBooktoLibrary("Ben", "Recipes", 150, true);
 displayBooks();
 
+const addBookButton = document.body.querySelector(".new-book");
+const dialog = document.body.querySelector("dialog");
+const closeModalButton = document.body.querySelector("dialog .close")
+addBookButton.addEventListener("click", () => dialog.showModal());
+closeModalButton.addEventListener("click", () => dialog.close());
