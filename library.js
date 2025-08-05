53075ddde51962361cd5305e39955431a43d947f
gcollins <grant.collins@codeware.com>
2025-08-05 20:28:07 +0000

    complete add book functionality

--- library.js
+++ library.js
@@ -8,26 +8,28 @@ function Book(author, title, numPages, read) {
   this.read = read;
 }
 
-function addBooktoLibrary(author, title, numPages, read) {
+function addBookToLibrary(author, title, numPages, read) {
   const book = new Book(author, title, numPages, read);
   myLibrary.push(book);
+  addBook(book);
 }
 
-function addBook(bookGrid, book) {
+function addBook(book) {
+  const bookGrid = document.body.querySelector(".book-grid");
   const bookContainer = document.createElement("div");
-    const titleDisplay = document.createElement("p");
-    titleDisplay.textContent = "Title: " + book.title;
-    bookContainer.appendChild(titleDisplay);
-    const authorDisplay = document.createElement("p");
-    authorDisplay.textContent = "Author: " + book.author;
-    bookContainer.appendChild(authorDisplay);
-    const pageNumDisplay = document.createElement("p");
-    pageNumDisplay.textContent = "Number of Pages: " + book.numPages;
-    bookContainer.appendChild(pageNumDisplay);
-    const readDisplay = document.createElement("p");
-    readDisplay.textContent = book.read ? "Has been read" : "Has not been read";
-    bookContainer.appendChild(readDisplay);
-    bookGrid.appendChild(bookContainer);
+  const titleDisplay = document.createElement("p");
+  titleDisplay.textContent = "Title: " + book.title;
+  bookContainer.appendChild(titleDisplay);
+  const authorDisplay = document.createElement("p");
+  authorDisplay.textContent = "Author: " + book.author;
+  bookContainer.appendChild(authorDisplay);
+  const pageNumDisplay = document.createElement("p");
+  pageNumDisplay.textContent = "Number of Pages: " + book.numPages;
+  bookContainer.appendChild(pageNumDisplay);
+  const readDisplay = document.createElement("p");
+  readDisplay.textContent = book.read ? "Has been read" : "Has not been read";
+  bookContainer.appendChild(readDisplay);
+  bookGrid.appendChild(bookContainer);
 }
 function handleSubmit(event) {
   event.preventDefault();
@@ -36,19 +38,16 @@ function handleSubmit(event) {
   const author = formData.get('author');
   const numPages = formData.get('pages');
   const read = (formData.get('read') === 'on');
-  addBooktoLibrary(author, title, pages, read);
+  addBookToLibrary(title, author, numPages, read);
 }
 
-function displayBooks() {
-  const bookGrid = document.body.querySelector(".book-grid");
-  myLibrary.map((book) => {
-    addBook(bookGrid, book);
+function displayBooks() {  myLibrary.map((book) => {
+    addBook(book);
   })
 }
-addBooktoLibrary("Grant", "To Kill a Mockingbird", 357, false);
-addBooktoLibrary("Jason", "Cookbook", 10, true);
-addBooktoLibrary("Ben", "Recipes", 150, true);
-displayBooks();
+addBookToLibrary("Grant", "To Kill a Mockingbird", 357, false);
+addBookToLibrary("Jason", "Cookbook", 10, true);
+addBookToLibrary("Ben", "Recipes", 150, true);
 
 const addBookButton = document.body.querySelector(".new-book");
 const dialog = document.body.querySelector("dialog");
